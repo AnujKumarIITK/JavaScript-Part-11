7fad197756e7d86976960424dc99454b59beceaf
Anuj Kumar <anujkumarbro002@gmail.com>
2025-08-20 21:33:39 +0530

    Explore Asynchrnous Nature of JS

--- app.js
+++ app.js
@@ -2,10 +2,24 @@
 
 // JS is a single threaded language.
 
+// let a = 25;
+// console.log(a);
+
+// let b = 10;
+// console.log(b);
+
+// console.log(a+b);
+
+//Synchronous Nature => Code executes line by line
+//ASynchronous Nature => Code executes not in lin by line.
+
 setTimeout(() =>{
     console.log("apna college");
 }, 2000);
 
-console.log("hello....");
+setTimeout(() =>{
+    console.log("Hello World!");
+}, 2000);
 
+console.log("hello....");
 
