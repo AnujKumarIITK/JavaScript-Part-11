c2037204548aa1c1fc0e981a79878eb340b66f95
Anuj Kumar <anujkumarbro002@gmail.com>
2025-08-22 00:22:07 +0530

    Callback nesting => CallBack Hell

--- app.js
+++ app.js
@@ -4,16 +4,22 @@
 
 h1 = document.querySelector("h1");
 
-function changeColor(color, delay) {
+function changeColor(color, delay, nextColorChange) {
     setTimeout(() => {
         h1.style.color = color;
+        if (nextColorChange) nextColorChange();
     }, delay);
 }
 
-changeColor("red", 1000);
+changeColor("red", 1000, () =>  {
+    changeColor("orange", 1000, () => {
+        changeColor("green", 1000, () => {
+            changeColor("yellow", 1000, () => {
+                changeColor("blue", 1000);
+            });
+        });
+    });
+});
 
-changeColor("orange", 2000);
 
-changeColor("green", 3000);
-
-
+// callbacks nesting => callback hell
